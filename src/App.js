e696411617185e16f0e32742ebdf97712890245f
GAURAV UPADHYAY <upadhyaygaurav450@gmail.com>
2023-09-25 23:10:16 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react"
 import './App.css'
 import Navbar from "./Navbar"
+import Body from "./Body"
+import WorkerSection from "./WorkerSection";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
      < Navbar/>
+     <Body/>
+     <WorkerSection/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/Body.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Body() {
+
+
+
+    return (
+        <div>
+            <div>
+                <section id="home" class="hero">
+                    <div class="hero-content">
+                        <h1>Find the Right Workers for Your Needs</h1>
+                        <p>Hire skilled professionals quickly and easily.</p>
+                        <div class="cta-buttons">
+                            <a href="#browse-workers" class="cta-button">Browse Workers</a>
+                            <a href="#post-job" class="cta-button">Post a Job</a>
+                        </div>
+                    </div>
+                   
+                </section>
+            </div>
+           
+        </div>
+    )
+
+
+}
